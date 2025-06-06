ef23beec8be6cab4ad01447343d21b5505aa8177
André Rogério Bononi <61595447+arbononi@users.noreply.github.com>
2025-06-06 20:59:04 -0300

    ProjetoHotel
    Projeto Hotel

---
+++ ProjetoConsoleCSharp/ProjetoHotel/Model/Reserva.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoHotel.Model
+{
+    public class Reserva
+    {
+        public int Id { get; set; }
+        public int Quarto { get; set; }
+        public string NomeDoHospede { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public DateTime DataCheckin { get; set; }
+        public DateTime DataCheckout { get; set; }
+    }
+}
